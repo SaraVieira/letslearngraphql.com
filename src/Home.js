33b65c8904a91b8836e25e40174ffc359f9fe284
Sara Vieira <hey@iamsaravieira.com>
2018-08-04 16:56:04 +0100

    start

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Home.css';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className="Home-header">
+          <h2>Welcome to letslearngraphql.com  </h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
